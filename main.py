d061f0d2aad6cc406c8578bba6522e9665b7048e
anudeike <ikechukwuanude@gmail.com>
2021-03-26 03:34:50 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# import the necessary packages
+import pickle
+import botometer
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import numpy as np
+import sqlite3
+import aspect_based_sentiment_analysis as absa
+
+# This is a sample Python script.
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Twitter Aspect Sentiment Analysis
+
+This is intended to isolate the aspect analysis from cleaning
+and preprocessing steps
