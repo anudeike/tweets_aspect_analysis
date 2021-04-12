a68d4c19b0abd11a33cbbdf8079f9d62f889c8ad
anudeike <ikechukwuanude@gmail.com>
2021-04-12 02:15:06 -0500

    should finally work

---
+++ apsa_from_classification_bank/AccountClassifer.py
@@ -0,0 +1,173 @@
+# import the necessary packages
+import pickle
+import botometer
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import numpy as np
+import sqlite3
+
+
+class AccountClassifierAnalyzer:
+    def __init__(self, rapid_api_key, twitter_app_auth, model_path, database_path, nlp, path_to_classification_bank=None, human_only=True):
+
+        # init the botometer module
+        self.tweet_index = 0
+
+        self.nlp = nlp
+
+        self.bom = botometer.Botometer(wait_on_ratelimit=True,
+                                       rapid_api_key=rapid_api_key,
+                                       **twitter_app_auth)
+
+        # load the model
+        self.model = self.load_model(model_path)
+
+        # load the bank - only if it exists
+        if path_to_classification_bank and len(path_to_classification_bank) > 0:
+            self.classification_bank = pd.read_csv(path_to_classification_bank)
+
+
+        # get the database connection
+        self.database_connection = sqlite3.connect(database_path)
+
+        # setting human only
+        self.human_only = human_only
+
+
+    def load_model(self, path):
+        """
+        Loads the model from a certain path.
+        :param path: Path to model created by pkl
+        :return: Model
+        """
+
+        try:
+            pl = pickle.load(open(path, 'rb'))
+            return pl
+        except Exception as e:
+            print(f'ERROR: {repr(e)}')
+
+    def fetch_from_classification_bank(self, username):
+
+        if self.classification_bank is None:
+            raise FileNotFoundError("Classification bank was not found. Please include a path to a classification bank.")
+
+        df = self.classification_bank
+
+        # search
+        res = df.loc[df['id'] == username]
+
+        if res.empty:
+            print(f'({self.tweet_index}): \n{username} could not be found in database.')
+            return None
+
+        pred = res.values
+
+        print(f'({self.tweet_index}): \n{username} fetched from classification bank.')
+
+        return pred[0][1]
+
+    def get_sentiment(self, text):
+        # get the sentiment in all of the target words
+
+        vaccine, virus, vaccines, vaccination = self.nlp(text, aspects=["vaccine", "virus", "vaccines", "vaccination"])
+
+        return [{
+            "vaccine_scores": vaccine.scores,
+            "vaccine_overall_sent": vaccine.sentiment
+        }, {
+            "virus_scores": virus.scores,
+            "virus_overall_sent": virus.sentiment
+        }, {
+            "vaccines_scores": vaccines.scores,
+            "vaccines_overall_sent": vaccines.sentiment
+        }, {
+            "vaccination_scores": vaccination.scores,
+            "vaccination_overall_sent": vaccination.sentiment
+        }]
+
+    def run(self, row):
+
+        try:
+
+            # turn into dictionary
+            #row = row.to_dict(orient='records')
+            # classify the account
+            # this fetches from the classification bank
+            screen_name = row['User ID']
+
+            # the fetched classification
+            fetched = self.fetch_from_classification_bank(screen_name)
+
+            if fetched is None:
+
+                # for now we just skip if it is not in the classification bank
+                print(f'({self.tweet_index}): Since {screen_name} was not found in the classification bank, it will be skipped.\n\n')
+                self.tweet_index += 1
+            else:
+                # if it is in the bank
+                print(f'({self.tweet_index}): Prediction for {screen_name}: {fetched}')
+
+                # if non-human
+                if int(fetched) == 0 and self.human_only == True:
+                    # not counting non humans
+                    print(
+                        f'({self.tweet_index}): {screen_name} has been classified as a non-human and therefore will not be counted in the final analysis.')
+                    self.tweet_index += 1
+                    return None
+
+                else:
+                    row["prediction"] = fetched # should only be humans
+
+            processed_tweet_content = row["proccessed_tweet"]
+
+            # get the sentiment
+            sent = self.get_sentiment(processed_tweet_content)
+            print(f'Sentiment Analysis Result: {sent}')
+
+            # vaccine scores
+            row["vaccine_score_neutral"] = sent[0]["vaccine_scores"][0]
+            row["vaccine_score_negative"] = sent[0]["vaccine_scores"][1]
+            row["vaccine_score_positive"] = sent[0]["vaccine_scores"][2]
+            row["vaccine_overall_sentiment"] = sent[0]["vaccine_overall_sent"]
+
+            # virus scores
+            row["virus_scores_neutral"] = sent[1]["virus_scores"][0]
+            row["virus_scores_negative"] = sent[1]["virus_scores"][1]
+            row["virus_scores_positive"] = sent[1]["virus_scores"][2]
+            row["virus_overall_sentiment"] = sent[1]["virus_overall_sent"]
+
+            # vaccines scores
+            row["vaccines_scores_neutral"] = sent[2]["vaccines_scores"][0]
+            row["vaccines_scores_negative"] = sent[2]["vaccines_scores"][1]
+            row["vaccines_scores_positive"] = sent[2]["vaccines_scores"][2]
+            row["vaccines_overall_sentiment"] = sent[2]["vaccines_overall_sent"]
+
+            # vaccination scores
+            row["vaccination_scores_neutral"] = sent[3]["vaccination_scores"][0]
+            row["vaccination_scores_negative"] = sent[3]["vaccination_scores"][1]
+            row["vaccination_scores_positive"] = sent[3]["vaccination_scores"][2]
+            row["vaccination_overall_sentiment"] = sent[3]["vaccination_overall_sent"]
+
+
+
+            print(f'Processed tweet # {self.tweet_index}')
+
+            # put the row into the database
+            row_dict = row.to_dict()
+            row = pd.DataFrame(row_dict, index=[row_dict['No.']])
+
+            row.to_sql(name="tweet_info_4", con=self.database_connection, if_exists="append", index=False)
+            self.database_connection.commit()
+
+            # increments
+            self.tweet_index += 1
+
+            return row
+
+        except Exception as e:
+            print(f"[top-level]: {repr(e)}\n")
+            return None
+        pass
+
---
+++ apsa_from_classification_bank/analyze.py
@@ -0,0 +1,137 @@
+# import the necessary packages
+from AccountClassifer import AccountClassifierAnalyzer
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import numpy as np
+import sqlite3
+import sys
+import json
+import aspect_based_sentiment_analysis as absa
+
+load_dotenv()
+nlp = absa.load()
+
+"""
+To use this file you will need a couple of things:
+1. Twitter Data in the form with the required columns (will be in the readme)
+2. The XGBoost Model File (should by a file made using the pickle package)
+3. An env file that contains your twitter credentials and your twitter credentials
+4. 
+"""
+
+
+def get_tokens_from_env():
+    """
+    Get the tokens from the env file located in this directory
+    :return: rapid_api_key, twitter_keys_object
+    """
+    no_env_error_message = """It doesn't seem like I can find your .env file.
+    Please make sure that it is in the same directory as this file and make sure that the
+    variables are labeled accord to the README."""
+
+    if (rapid_api_key := os.getenv('RAPID_FIRE_KEY')) == None:
+        # raise an error
+        raise ValueError(f'{no_env_error_message}\n\nCould not find variable RAPID_FIRE_KEY.')
+
+    if (twitter_consumer_key := os.getenv('TWITTER_API_KEY')) == None:
+        # raise an error
+        raise ValueError(f'{no_env_error_message}\n\nCould not find variable TWITTER_API_KEY.')
+
+    if (twitter_consumer_secret := os.getenv('TWITTER_API_SECRET')) == None:
+        # raise an error
+        raise ValueError(f'{no_env_error_message}\n\nCould not find variable TWITTER_API_SECRET.')
+
+    # if there are no errors then you can go ahead and return all the keys
+    twitter_app_auth = {
+        'consumer_key': twitter_consumer_key,
+        'consumer_secret': twitter_consumer_secret,
+    }
+
+    return rapid_api_key, twitter_app_auth
+
+def get_tweet_dataframe(file_path, num_rows, start_index, skip_error_lines=True):
+    """
+    Returns a dataframe of the selected rows
+    :param file_path: path to the csv file
+    :param num_rows: number of rows to read from
+    :param start_index: starting index
+    :param skip_error_lines: If this is False, then lines that cause errors will be dropped.
+    :return: dictionary
+    """
+
+    # make sure to only read info within the desired range
+    def skip_function(ind):
+
+        # if the count is zero then just return true
+        if num_rows == 0 or start_index == 0:
+            return False
+
+        if start_index + num_rows > ind > start_index:
+            return True
+
+        return False
+
+    if num_rows != 0:
+        print(f"Reading rows {start_index} through {start_index + num_rows}...")
+    else:
+        print("Reading all available rows...")
+
+    # read from the csv
+    # names="No.,Is retweet?,Tweet ID,Post Date,User Display Name,User ID,User Info,User Location,Tweet Content,Number of Quotes,Number of Replies,Number of Likes,Number of Retweets,Raw link 1,Solved link 1,Raw link 2,Solved link 2,Raw link 3,Solved link 3,Raw link 4,Solved link 4,Raw link 5,Solved link 5,proccessed_tweet".split(',')
+    df = pd.read_csv(file_path, skiprows=lambda x: skip_function(x), error_bad_lines=skip_error_lines)
+
+    return df
+
+
+def run_analyzer(preprocessed_tweet_data_file_path, model_path, database_path, classification_bank_path, count=0, start_index=0, human_only=True):
+
+    print(f'Received: {preprocessed_tweet_data_file_path, model_path, database_path}')
+
+    # get the keys and tokens from an env file
+    rapid_api_token, twitter_app_auth_tokens = get_tokens_from_env()
+
+    # get an object to do the classification
+    # this contains the botometer object
+    account_classifier = AccountClassifierAnalyzer(rapid_api_key=rapid_api_token,
+                                                   twitter_app_auth=twitter_app_auth_tokens,
+                                                   model_path=model_path,
+                                                   database_path=database_path,
+                                                   path_to_classification_bank=classification_bank_path,
+                                                   human_only=human_only,
+                                                   nlp=nlp)
+
+
+    # get the dataframe of all the tweets
+    tweet_df = get_tweet_dataframe(file_path=preprocessed_tweet_data_file_path,
+                        num_rows=count,
+                        start_index=start_index)
+
+    # turn into an array of dictionaries
+    # tweets_dict_array = tweet_df.to_dict(orient='records')
+    # print(tweets_dict_array)
+    # exit(9)
+    # use df.apply to cycle through each dataframe
+    tweet_df.apply(account_classifier.run, axis=1)
+
+
+
+
+if __name__ == '__main__':
+
+    # import a json config file
+    config_file = "config.json"
+    f = open(config_file)
+
+    # turn into a dictionary and get all the necessary values
+    config_data = json.load(f)
+
+    # run the analyzer
+    run_analyzer(preprocessed_tweet_data_file_path=config_data['tweet_data_path'],
+                 classification_bank_path=config_data['classification_bank_path'],
+                 model_path=config_data["model_file_path"],
+                 database_path=config_data["database_file_path"],
+                 count=config_data["count"],
+                 start_index=config_data["start_index"],
+                 human_only=config_data["human_only"])
+
